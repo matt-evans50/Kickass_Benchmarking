fd1e6a318f324d3cd36e228227948c9f2b3dbbd9
Caleb Young <menlocaleb@gmail.com>
2014-05-29 22:49:01 -0500

    Added command line options for threaded_server.py

--- threaded_server.py
+++ threaded_server.py
@@ -8,6 +8,9 @@ import socket
 import time
 import random
 import math
+import argparse
+
+
 
 def createTraffic(dataSize):
     inputString = ''
@@ -16,24 +19,40 @@ def createTraffic(dataSize):
     inputString += '0'
     return inputString
 
+def runServer(args):
+    host = '' 
+    backlog = 5 
+    size = 1024 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.bind((host,args.port)) 
+    s.listen(backlog) 
+    while 1:
+        client, address = s.accept() 
+        data = client.recv(size).decode()
+        print('got data\n')
+        breakup = data.split('//') # second field should be size of HTTP request
+        if (breakup[0] == 'GET http:'):
+            print('registered HTTP request')
+            client.send(createTraffic(breakup[1]).encode())
+        else: 
+            client.send(data.encode())
+        # close connection after responding to data
+        client.close()
 
-host = '' 
-port = 8000 
-backlog = 5 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind((host,port)) 
-s.listen(backlog) 
 
-while 1:
-    client, address = s.accept() 
-    data = client.recv(size).decode()
-    print('got data\n')
-    breakup = data.split('//') # second field should be size of HTTP request
-    if (breakup[0] == 'GET http:'):
-        print('registered HTTP request')
-        client.send(createTraffic(breakup[1]).encode())
-    else: 
-        client.send(data.encode())
-    # close connection after responding to data
-    client.close()
+def handleCommandLineArguments():
+
+    parser = argparse.ArgumentParser(description = 'Start server for threaded client for Kickass testing.')
+    # port: port to connect to
+    parser.add_argument('-p', '--port', type=int, default=8000)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    # handle command line args, this function will quit program if error in options
+    args = handleCommandLineArguments()
+
+    runServer(args);
+
+
