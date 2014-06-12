55ce989b6d322f977cd42bcc2abd5f6d138f23bf
Caleb Young <menlocaleb@gmail.com>
2014-06-12 05:51:30 -0500

    adding matts version of threaded_client for merging into mine

--- threaded_client.py
+++ threaded_client.py
@@ -6,71 +6,89 @@ import random
 import math
 from multiprocessing import Process
 import argparse               # example for using this module: https://docs.python.org/3.4/library/argparse.html#module-argparse
+import matplotlib.pyplot as plt
+import numpy as np
 
 host = 'localhost'          # Get local machine name
 port = 8000               # Reserve a port for your service.
 iterations = 2           # number of times each client sends data
 numProcs = 4               # number of processes to run
-fileName = 'client-timing-'
 args = None
 
 
 def createHTTPrequest(alpha, randMode):
     # outputs in the format: 'GET http://[number of bytes]
     if (randMode == 'pareto'): # pareto distribution
-        inputSize = math.ceil(100*random.paretovariate(alpha))
+        inputSize = math.ceil(10000*random.paretovariate(alpha))
     elif (randMode == 'rand'): # uniform distribution
-        inputSize = math.ceil(100*random.randrange(400))
+        inputSize = math.ceil(10000*random.randrange(400))
     elif (randMode == 'exp'): #exponential distribution
-        inputSize = math.ceil(100*random.expovariate(1/200))
+        inputSize = math.ceil(10000*random.expovariate(1/200))
     return 'GET http://' + str(int(inputSize))
 
-def createTraffic(alpha, randMode):
-    # creates a random number of bytes ('1's)ending in a '0'
-    if (randMode == 1): # pareto distribution
-        inputSize = math.ceil(100*random.paretovariate(alpha))
-    elif (randMode == 2): # uniform distribution
-        inputSize = math.ceil(100*random.randrange(400))
-    elif (randMode == 3): #exponential distribution
-        inputSize = math.ceil(100*random.expovariate(1/200))
-    inputString = ''
-    for i in range(inputSize):
-        inputString += '1'
-    inputString += '0'
-    return inputString
 
 def getWaitTime(meanWaitTime):
     waitlen = random.expovariate(1/meanWaitTime)
     return waitlen
 
 
-
 def createClient(iterations, id):
+    xvals = [] # number of bytes transferred
+    yvals = [] # flow completion time
+    tvals = [] # times at which 1024 bytes have been sent
+    zvals = [] # integer number line representing num of packet sent 
     for i in range(0,iterations):
-        goodFile = False
-        try:
-            f = open(fileName + str(id), 'w')
-            goodFile = True
-        except IOError as e:
-            print "I/O error({0}): {1}".format(e.errno, e.strerror) 
+        print('iteration', i, 'of', iterations)
         s = socket.socket()         # Create a socket object
-        print(str(id) + ' connecting to ' + str(args.dest) + ':' + str(args.port) + '...')
+        #print(str(id) + ' connecting to ' + str(args.dest) + ':' + str(args.port) + '...')
+        s.setsockopt(socket.IPPTROTO_TCO, 13, "reno")        
         s.connect((args.dest, args.port))
-        print('connection successful.')
-        print('send HTTP request:')
+        #print('connection successful.')
+        #print('send HTTP request:')
         inputString = createHTTPrequest(2, args.sizeDist)
-        print(inputString)
+        breakup = inputString.split('//')
+        numBytes = int(breakup[1])
+        xvals = xvals + [numBytes]
+        #print(inputString)
         r=inputString
-        start_time = time.time()
         s.send(r.encode())
+        packetNo = 1 # index used to keep track of number of packets
+        start_time = time.time()
         data = s.recv(1024).decode()
-        elapsed = time.time() - start_time
-        print('elapsed time: ', elapsed)
+        #start_time = time.time()
+        while data[-1] != '0':
+            elapsed = (time.time() - start_time)*1000 # figure out tval
+            #print(elapsed)
+            tvals = tvals + [elapsed]
+            zvals = zvals + [packetNo]
+            data = s.recv(1024).decode()
+            #time.sleep(0.0001)
+            packetNo += 1
+        elapsed = (time.time() - start_time)*1000 # puts in ms
+        yvals = yvals + [elapsed]
+        #print('elapsed time: ', elapsed, 'ms')
         s.close()
-        print('socket closed')
-        f.write(str(start_time) + '\t' + str(elapsed) + '\t')
-        f.close()
+        #print('socket closed')
         time.sleep(getWaitTime(1))
+    '''
+    plt.plot(zvals, tvals)
+    plt.xlabel('Number of Packets Transferred (1024B)')
+    plt.ylabel('Elapsed Transmission Time (ms)')
+    plt.title('The Life of a Flow')    
+    plt.axis([0, max(zvals), 0, max(tvals)])
+    #plt.show()    
+    
+    #x = np.arange(xvals)
+    '''
+    plt.plot(xvals, yvals, linestyle="none", marker=".")
+    plt.xlabel('Number of Bytes Transferred')
+    plt.ylabel('Transmission Time (ms)')
+    plt.title('Transfer Time based on Message Size')    
+    plt.axis([min(xvals), max(xvals), min(yvals), max(yvals)])
+    plt.show()
+    
+    #print(xvals)
+    #print(yvals)
 
 
 def handleCommandLineArguments():
@@ -106,8 +124,3 @@ if __name__ == '__main__':
     for p in processes:
         p.join()
     print("All clients done.");
-
-
-
-
-            
